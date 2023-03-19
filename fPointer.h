b365499df70b81e7d905f08396ec8794c80d6120
LeoTkach <leonidt1908@gmail.com>
2023-03-19 23:00:40 +0200

    Create fPointer.h

---
+++ fPointer.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <stdio.h>
+using namespace std;
+
+void printFile(const char* filename);
+
+void appendtoFile(const char* filename);
+
+void rearrangeChars(const char* filename);
+
+void copyOddLines(const char* filename);
+
+void removeCharFromFile(const char* filename);
+
+void createFile(const char* filename);
+
+void fileProcessing();
