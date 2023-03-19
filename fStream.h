8fc699c34b89bb5a5c8fdce7b632121579f7d804
LeoTkach <leonidt1908@gmail.com>
2023-03-19 23:00:38 +0200

    Create fStream.h

---
+++ fStream.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <stdio.h>
+#include <fstream>
+using namespace std;
+
+void printFile1(string &filename);
+
+void appendtoFile1(string &filename);
+
+void rearrangeChars1(string &filename);
+
+void copyOddLines1(string &filename);
+
+void removeCharFromFile1(string &filename);
+
+void createFile1(string &filename);
+
+void fileProcessing1();
