979dd0197cda5f225a0f41e18b58e7c079bd55d5
LeoTkach <leonidt1908@gmail.com>
2023-04-02 20:31:18 +0300

    Update main.cpp

--- OP2_laba2/main.cpp
+++ OP2_laba2/main.cpp
@@ -3,7 +3,7 @@
 int main(){
     char ans;
     do{
-        fileProcessing();
+        binaryFileProcessing();
         cout << "Do you want to restart the programm ? (If yes - press y or any other letter to exit the programm)" ;
         cin.ignore();
         cin >> ans;
