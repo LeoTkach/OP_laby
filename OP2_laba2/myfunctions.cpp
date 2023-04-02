ff5b785fcf4958d870ada58efbe52b927a050162
LeoTkach <leonidt1908@gmail.com>
2023-04-02 20:51:42 +0300

    Update myfunctions.cpp

--- OP2_laba2/myfunctions.cpp
+++ OP2_laba2/myfunctions.cpp
@@ -6,13 +6,15 @@
 #include <iomanip>
 using namespace std;
 
+// створення нового бінарного файлу
 void createNewBinaryFile(string &filename) {
     ofstream file(filename,ios::binary);
     file.close();
     cout << "File " << filename << " has been successfully created!" << endl;
 }
 
-void readCarsFromBinaryFile(string &filename) {
+// зчитуваня та виведення списку машин та їх харакетристик, які наявні у файлі
+void readAllCarsFromBinaryFile(string &filename) {
     ifstream file(filename, ios::binary);
     Car car;
     cout << setw(20) << left << "Car name" << setw(17) << right << "Manufacture date" << setw(19) << right << "Reception date" << endl;
@@ -32,6 +34,7 @@ void readCarsFromBinaryFile(string &filename) {
     file.close();
 }
 
+// отримання сьогоднішньої дати
 Date getCurrentDate() {
     time_t now = time(0);
     tm* currentTime = localtime(&now);
@@ -43,6 +46,7 @@ Date getCurrentDate() {
     return currentDate;
 }
 
+// перевірка введеної дати на валідність
 bool isDateValid(int day, int month, int year, int day1=0, int month1=0, int year1=0) {
     if (month < 1 || month > 12) {
         cout<<"Wrong date!"<<endl;
@@ -94,6 +98,7 @@ bool isDateValid(int day, int month, int year, int day1=0, int month1=0, int yea
     return true;
 }
 
+// додавання у бінарний файл записів про нову машину
 void addCarToBinaryFile(string &filename) {
     char ans;
     do{
@@ -113,38 +118,40 @@ void addCarToBinaryFile(string &filename) {
                 cout << "Enter the date of reception (day month year): ";
                 cin >> car.recepdate.day >> car.recepdate.month >> car.recepdate.year;
             }while(isDateValid(car.recepdate.day, car.recepdate.month, car.recepdate.year, car.manudate.day, car.manudate.month, car.manudate.year)==false);
-            // записываем машину в файл
-            // сначала записываем длину имени машины
+            // записуємо машину у файл
+            // записуємо доавжину імені
             int namelength = car.name.size();
             file.write(reinterpret_cast<const char*>(&namelength), sizeof(int));
-            // затем записываем само имя
+            // записуємо ім'я
             file.write(car.name.c_str(), namelength);
-            // затем записываем даты
+            // записуємо дату створення та отримання для продажу
             file.write(reinterpret_cast<const char*>(&car.manudate), sizeof(Date));
             file.write(reinterpret_cast<const char*>(&car.recepdate), sizeof(Date));
 
             cout << "\nCar successfully added to the file " << filename << endl;
             file.close();
-            readCarsFromBinaryFile(filename);
+            readAllCarsFromBinaryFile(filename);
         }  
     }while(ans=='y');
 }
 
-void carsReceivedInLastMonth(string &filename) {
+// виведення списку машин, які було отримано для продажу у останній місяць 
+void readCarsReceivedInLastMonth(string &filename) {
     Date currentDate = getCurrentDate();
     ifstream file(filename, ios::binary);
-    // read cars from file and output those sold in the given month
     Car car;
     cout <<"\nList of cars received for sale in last month:"<<endl;
     cout << setw(20) << left << "Car name" << setw(17) << right << "Manufacture date" << setw(19) << right << "Reception date" << endl;
     int namelength=car.name.size();
     while (file.read(reinterpret_cast<char*>(&namelength), sizeof(int))) {
+        // зчитування імені
         string name(namelength, '\0');
         file.read(&name[0], namelength);
         car.name = name;
+        // зчитування дат
         file.read(reinterpret_cast<char*>(&car.manudate), sizeof(Date));
         file.read(reinterpret_cast<char*>(&car.recepdate), sizeof(Date));
-        // check if car was sold in the given month and output its info
+        // у разі виконання вимог виведеннч на екран
         if ((car.recepdate.month == currentDate.month)&&(car.recepdate.year == currentDate.year)) {
             cout << setw(24) << left << car.name 
             << setw(2) << right << car.manudate.day << "." << setw(2) << right << car.manudate.month << "." << setw(4) << right << car.manudate.year
@@ -155,20 +162,22 @@ void carsReceivedInLastMonth(string &filename) {
     file.close();
 }
 
-void carsReleasedMoreThanYear(string &filename) {
+// виведення списку машин, які були випущені більш ніж за рік до надходження у продаж 
+void readCarsReleasedMoreThanYear(string &filename) {
     ifstream file(filename, ios::binary);
-    // read cars from file and output those sold in the given month
     Car car;
     cout <<"\nList of the cars released more than a year before coming to sale:"<<endl;
     cout << setw(20) << left << "Car name" << setw(17) << right << "Manufacture date" << setw(19) << right << "Reception date" << endl;
     int namelength=car.name.size();
     while (file.read(reinterpret_cast<char*>(&namelength), sizeof(int))) {
+        // зчитування імені
         string name(namelength, '\0');
         file.read(&name[0], namelength);
         car.name = name;
+        // зчитування дат
         file.read(reinterpret_cast<char*>(&car.manudate), sizeof(Date));
         file.read(reinterpret_cast<char*>(&car.recepdate), sizeof(Date));
-        // check if car was sold in the given month and output its info
+        // у разі виконання вимог виведеннч на екран
         if (car.recepdate.year - car.manudate.year > 1) {
             cout << setw(24) << left << car.name 
             << setw(2) << right << car.manudate.day << "." << setw(2) << right << car.manudate.month << "." << setw(4) << right << car.manudate.year
@@ -179,7 +188,7 @@ void carsReleasedMoreThanYear(string &filename) {
     file.close();
 }
 
-//обробка файлу згідно умов завдання
+// обробка файлу згідно умов завдання
 void binaryFileProcessing() {
     string filename;
     cout << "Enter name of the file without extension: ";
@@ -190,15 +199,15 @@ void binaryFileProcessing() {
         file.close();
         createNewBinaryFile(filename);
         addCarToBinaryFile(filename);
-        carsReceivedInLastMonth(filename);
-        carsReleasedMoreThanYear(filename);
+        readCarsReceivedInLastMonth(filename);
+        readCarsReleasedMoreThanYear(filename);
     } else { 
         cout << "\nFile " << filename << " already exists. " << endl;
         cout << "\nHere is list of cars this binary file contains: " << endl;
         file.close();
-        readCarsFromBinaryFile(filename);
+        readAllCarsFromBinaryFile(filename);
         addCarToBinaryFile(filename);
-        carsReceivedInLastMonth(filename);
-        carsReleasedMoreThanYear(filename);
+        readCarsReceivedInLastMonth(filename);
+        readCarsReleasedMoreThanYear(filename);
     }
 }
