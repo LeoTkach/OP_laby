ff7f19aece4faeb0375a196067b83861776d2557
LeoTkach <leonidt1908@gmail.com>
2023-04-02 15:53:58 +0300

    Update main.cpp

--- OP2_laba2/main.cpp
+++ OP2_laba2/main.cpp
@@ -1,6 +1,12 @@
 #include "myfunctions.h"
 
 int main(){
-    fileProcessing();
+    char ans;
+    do{
+        fileProcessing();
+        cout << "Do you want to restart the programm ? (If yes - press y or any other letter to exit the programm)" ;
+        cin.ignore();
+        cin >> ans;
+    }while(ans=='y');
     return 0;
 }
