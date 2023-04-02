eebd7b4309f9b9da7d2ba968fb597e7728015eb7
LeoTkach <leonidt1908@gmail.com>
2023-04-02 20:51:45 +0300

    Update myfunctions.h

--- OP2_laba2/myfunctions.h
+++ OP2_laba2/myfunctions.h
@@ -6,30 +6,40 @@
 #include <iomanip>
 using namespace std;
 
+// структура для збереження дат
 struct Date{
     int day;
     int month;
     int year;
 };
 
+// структура для збереження характеристик машин
 struct Car{
     string name;
     Date manudate;
     Date recepdate; 
 };
 
+// створення нового бінарного файлу
 void createNewBinaryFile(string &filename);
 
-void readCarsFromBinaryFile(string &filename);
+// зчитуваня та виведення списку машин та їх харакетристик, які наявні у файлі
+void readAllCarsFromBinaryFile(string &filename);
 
+// отримання сьогоднішньої дати
 Date getCurrentDate();
 
+// перевірка введеної дати на валідність
 bool isDateValid(int day, int month, int year, int day1, int month1, int year1);
 
+// додавання у бінарний файл записів про нову машину
 void addCarToBinaryFile(string &filename);
 
-void carsReceivedInLastMonth(string &filename);
+// виведення списку машин, які було отримано для продажу у останній місяць 
+void readCarsReceivedInLastMonth(string &filename);
 
-void carsReleasedMoreThanYear(string &filename);
+// виведення списку машин, які були випущені більш ніж за рік до надходження у продаж 
+void readCarsReleasedMoreThanYear(string &filename);
 
+// обробка файлу згідно умов завдання
 void binaryFileProcessing();
