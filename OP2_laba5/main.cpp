975392b6c9b2365aa6b60d4b4a9b2a15e2b7d420
LeoTkach <leonidt1908@gmail.com>
2023-04-29 22:42:15 +0300

    Update main.cpp

--- OP2_laba5/main.cpp
+++ OP2_laba5/main.cpp
@@ -4,7 +4,7 @@ int main(){
     cout<<"Enter the total number of triangles of all types: ";
     int n;
     cin>>n;
-    TTriangle Triangles[n];
+    TTriangle *Triangles[n];
     int i;
     float Perim=0;
     float Square=0;
@@ -13,24 +13,24 @@ int main(){
         float Ax_, Ay_, Bx_, By_, Cx_, Cy_;
         cout<<"Enter coordinates of the edges of the triangle(Ax, Ay, Bx, By, Cx, Cy): ";
         cin>>Ax_>>Ay_>>Bx_>>By_>>Cx_>>Cy_;
-        Triangles[i]=IsoTriangle (Ax_, Ay_, Bx_, By_, Cx_, Cy_);
-        Perim+=Triangles[i].calcP();
+        Triangles[i]=new IsoTriangle (Ax_, Ay_, Bx_, By_, Cx_, Cy_);
+        Perim+=Triangles[i]->calcP();
     }
     cout<<"Equilateral triangles: "<<endl;
     for(i=i;i<(n/3)*2+((n-(n/3)*3)/2)*2+(n-(n/3)*3)%2;i++){
         float Ax_, Ay_, Bx_, By_, Cx_, Cy_;
         cout<<"Enter coordinates of the edges of the triangle(Ax, Ay, Bx, By, Cx, Cy): ";
         cin>>Ax_>>Ay_>>Bx_>>By_>>Cx_>>Cy_;
-        Triangles[i]=EquTriangle (Ax_, Ay_, Bx_, By_, Cx_, Cy_);
-        Square+=Triangles[i].calcS();
+        Triangles[i]=new EquTriangle (Ax_, Ay_, Bx_, By_, Cx_, Cy_);
+        Square+=Triangles[i]->calcS();
     }
     cout<<"Right triangles: "<<endl;
     for(i=i;i<n;i++){
         float Ax_, Ay_, Bx_, By_, Cx_, Cy_;
         cout<<"Enter coordinates of the edges of the triangle(Ax, Ay, Bx, By, Cx, Cy): ";
         cin>>Ax_>>Ay_>>Bx_>>By_>>Cx_>>Cy_;
-        Triangles[i]=RTriangle (Ax_, Ay_, Bx_, By_, Cx_, Cy_);
-        Square+=Triangles[i].calcS();
+        Triangles[i]=new RTriangle (Ax_, Ay_, Bx_, By_, Cx_, Cy_);
+        Square+=Triangles[i]->calcS();
     }
     cout<<"The total perimeter of all isosceles triangles: "<<Perim<<endl;
     cout<<"The total area of all equilateral and right triangles: "<<Square<<endl;
