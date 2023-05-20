e7aecfedcd0d6acba7bf0fd7393a16b8ab4b6a28
LeoTkach <leonidt1908@gmail.com>
2023-05-20 14:33:02 +0300

    Update main.cpp

--- OP2_laba6/main.cpp
+++ OP2_laba6/main.cpp
@@ -7,21 +7,25 @@ using namespace std;
 
 
 int main() {
-    cout << "Enter the type of the Doubly Linked List (int, double, char, string): ";
-    string type;
-    cin >> type;
 
-    if (type == "int") {
-        processDoublyLinkedList<int>();
-    } else if (type == "double") {
-        processDoublyLinkedList<double>();
-    } else if (type == "char") {
-        processDoublyLinkedList<char>();
-    } else if (type == "string") {
-        processDoublyLinkedList<string>();
-    } else {
-        cout << "Unsupported type" << endl;
-    }
+
+    string type;
+      
+    do{
+        cout << "Enter the type of the Doubly Linked List (int, double, char, string): ";
+        cin >> type;
+        if (type == "int") {
+            processDoublyLinkedList<int>();
+        } else if (type == "double") {
+            processDoublyLinkedList<double>();
+        } else if (type == "char") {
+            processDoublyLinkedList<char>();
+        } else if (type == "string") {
+            processDoublyLinkedList<string>();
+        } else {
+            cout << "Unsupported type" << endl;
+        }
+    }while(type!="int" && type!="double" && type!="char" && type!="string" );
 
     return 0;
 }
