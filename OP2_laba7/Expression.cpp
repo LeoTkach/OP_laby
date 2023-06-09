baf35c15bd1b98db9f9993f09b83f724519c3fe1
LeoTkach <leonidt1908@gmail.com>
2023-06-09 12:44:16 +0300

    Update Expression.cpp

--- OP2_laba7/Expression.cpp
+++ OP2_laba7/Expression.cpp
@@ -3,16 +3,16 @@
 
 Expression::Expression(double nValue, double mValue) {
     if (nValue < 0) {
-        throw gcnew System::Exception("The value of n must be non-negative");
+        throw "The value of n must be non-negative";
     }
     else if (std::floor(nValue) != nValue) {
-        throw gcnew System::Exception("The value of n must be a an integer");
+        throw "The value of n must be a an integer";
     }
     else if (nValue > 15) {
-        throw gcnew System::Exception("The value of n must not exceed 15");
+        throw "The value of n must not exceed 15";
     }
     else if (mValue > 1000000000 || mValue < -1000000000) {
-        throw gcnew System::Exception("The value of m must be within the limits [-10^9;10^9] ");
+        throw "The value of m must be within the limits [-10^9;10^9] ";
     }
     n = nValue;
     m = mValue;
